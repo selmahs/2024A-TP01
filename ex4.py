f77767abfaf69e5173fcc614a87490cfb2dec576
AyaBky <abenkay@hotmail.com>
2024-09-11 16:55:03 -0400

    Update ex4.py
    je t<Aime

--- ex4.py
+++ ex4.py
@@ -3,4 +3,17 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level =input("Veuillez saisir le pourcentage de batterie du bateau")
+battery_level=int(battery_level)
+if (battery_level >50)and(battery_level<=100):
+    print (2*battery_level)
+if(battery_level >25)and (battery_level<=50) :
+    print (0.5*battery_level)
+if(battery_level >10)and (battery_level<=25) :
+    print (1*battery_level)
+if(battery_level >5)and(battery_level<=10):
+    print (2.5*battery_level)
+if(battery_level>0) and(battery_level<=5):
+    print (6*battery_level)
+
+    
