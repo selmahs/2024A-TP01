2d23a8faef29f7f9a26ded75d3687be32054010f
selmahs <selmahseine@gmail.com>
2024-09-11 17:22:07 -0400

    Update ex5.py
    YAAYA

--- ex5.py
+++ ex5.py
@@ -3,10 +3,7 @@
 
 country = input("Pays concerne ?")
 code_medals = input("Chaine representant les medailles ?")
-G=0
-S=0
-B=0
-
+G= S =B =0
 for letter in code_medals:
     if letter == "G": G+=1
     if letter == "S": S+=1
