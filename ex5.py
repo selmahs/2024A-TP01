b3697ac4b572dc4b1030d1ad97cd697809a15d4e
selmahs <selmahseine@gmail.com>
2024-09-11 17:17:41 -0400

    Update ex5.py
    ta des beaux yeux

--- ex5.py
+++ ex5.py
@@ -1,5 +1,18 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+country = input("Pays concerne ?")
+code_medals = input("Chaine representant les medailles ?")
+G=0
+S=0
+B=0
+
+for letter in code_medals:
+    if letter == "G": G+=1
+    if letter == "S": S+=1
+    if letter == "B": B+=1
+
+print(f"""{country}:
+- {G} Or
+- {S} Argent 
+- {B} Bronze""")
